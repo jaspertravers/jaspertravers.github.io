ddf0920559acd49a55abf4caec96f33bd4a3cf47
simsinght <simsinght@gmail.com>
2016-05-28 22:46:38 -0700

    A lot of work. Mainly have a project 1 and the homepage. text needs to be  added but the template is more or less set up.

---
+++ assets/js/jasper.js
@@ -0,0 +1,63 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+var home_selected = true;
+var project_selected = false;
+
+function main() {
+    "use strict";
+    
+    $('#Home').mouseenter(function () {
+        $('#Home').css("background-color", "lightgray");
+    });
+    $('#Home').mouseleave(function () {
+        if (home_selected !== true) {
+            $('#Home').css("background-color", "whitesmoke");
+        }
+        if (home_selected === true) {
+            $('#Home').css("background-color", "darkgray");
+        }
+    });
+    
+    $('#Project1').mouseenter(function () {
+        $('#Project1').css("background-color", "lightgray");
+    });
+    
+    $('#Project1').mouseleave(function () {
+        if (project_selected !== true) {
+            $('#Project1').css("background-color", "whitesmoke");
+        }
+        
+        if (project_selected === true) {
+            $('#Project1').css("background-color", "darkgray");
+        }
+    });
+    
+    $('#Home').click(function () {
+        $('#Project1Container').hide();
+        $('#HomeContainer').show();
+        $('#Home').css("background-color", "darkgray");
+        $('#Project1').css("background-color", "whitesmoke");
+        home_selected = true;
+        project_selected = false;
+    });
+    
+    $('#Project1').click(function () {
+        $('#HomeContainer').hide();
+        $('#Project1Container').show();
+        $('#Home').css("background-color", "whitesmoke");
+        $('#Project1').css("background-color", "darkgray");
+        project_selected = true;
+        home_selected = false;
+    });
+    
+}
+
+
+
+$(document).ready(function () {
+    "use strict";
+    $('#Project1Container').hide();
+    $('#Home').css("background-color", "darkgray");
+    main();
+});
