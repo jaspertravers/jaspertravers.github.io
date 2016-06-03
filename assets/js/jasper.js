e2d666a2184cb0dbacc11db43041074392b2d6e2
simsinght <simsinght@gmail.com>
2016-06-03 00:34:01 -0700

    played around with three.js library. added code in jasper.js to handle a 3rd project once it's put in

--- assets/js/jasper.js
+++ assets/js/jasper.js
@@ -2,7 +2,8 @@
 /*global $, jQuery, alert*/
 
 var home_selected = false;
-var project_selected = true;
+var project1_selected = true;
+var project2_selected = false;
 
 function main() {
     "use strict";
@@ -24,30 +25,63 @@ function main() {
     });
     
     $('#Project1').mouseleave(function () {
-        if (project_selected !== true) {
+        if (project1_selected !== true) {
             $('#Project1').css("background-color", "whitesmoke");
         }
         
-        if (project_selected === true) {
+        if (project1_selected === true) {
             $('#Project1').css("background-color", "darkgray");
+        }
+    });
+    
+    /* DRAGGABLE CUBES MAN */
+    $('#Project2').mouseenter(function () {
+        $('#Project2').css("background-color", "lightgray");
+    });
+    
+    $('#Project2').mouseleave(function () {
+        if (project2_selected !== true) {
+            $('#Project2').css("background-color", "whitesmoke");
+        }
+        
+        if (project2_selected === true) {
+            $('#Project2').css("background-color", "darkgray");
         }
     });
     
     $('#Home').click(function () {
         $('#Project1Container').hide();
+        $('#Project2Container').hide();
         $('#HomeContainer').show();
         $('#Home').css("background-color", "darkgray");
         $('#Project1').css("background-color", "whitesmoke");
+        $('#Project2').css("background-color", "whitesmoke");
         home_selected = true;
-        project_selected = false;
+        project1_selected = false;
+        project2_selected = false;
     });
     
     $('#Project1').click(function () {
         $('#HomeContainer').hide();
+        $('#Project2Container').hide();
         $('#Project1Container').show();
         $('#Home').css("background-color", "whitesmoke");
         $('#Project1').css("background-color", "darkgray");
-        project_selected = true;
+        $('#Project2').css("background-color", "whitesmoke");
+        project1_selected = true;
+        project2_selected = false;
+        home_selected = false;
+    });
+    
+    $('#Project2').click(function () {
+        $('#HomeContainer').hide();
+        $('#Project1Container').hide();
+        $('#Project2Container').show();
+        $('#Home').css("background-color", "whitesmoke");
+        $('#Project1').css("background-color", "whitesmoke");
+        $('#Project2').css("background-color", "darkgray");
+        project2_selected = true;
+        project1_selected = false;
         home_selected = false;
     });
     
@@ -58,6 +92,7 @@ function main() {
 $(document).ready(function () {
     "use strict";
     $('#HomeContainer').hide();
+    $('#Project2Container').hide();
     $('#Project1').css("background-color", "darkgray");
     main();
 });
