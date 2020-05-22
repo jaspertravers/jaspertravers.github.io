38506b1c19fe48aef556118291cf957564c1ec5f
Jasper Travers <wtravers@ucsd.edu>
2020-05-22 11:11:05 -0700

    stopify09 demo

--- demos/board03/main.js
+++ demos/board03/main.js
@@ -9,83 +9,87 @@ let newpart;
 
 //
 //  run
-// 
+//
 window.addEventListener("load", onLoad);
 
 function onLoad() {
-    let {main, part, canvas} = bootstrap();
+  let { main, part, canvas } = bootstrap();
 }
 
-function bootstrap () {
-    //
-    // main
-    //
-    let main = document.createElement("main");
-    main.setAttribute("id", "main");
-    main.classList.add("main");
+function bootstrap() {
+  //
+  // main
+  //
+  let main = document.createElement("main");
+  main.setAttribute("id", "main");
+  main.classList.add("main");
 
-    //
-    // part
-    //
-    let part = document.createElement("div");
-    part.setAttribute("id", "default");
-    part.classList.add("part");
+  //
+  // part
+  //
+  let part = document.createElement("div");
+  part.setAttribute("id", "default");
+  part.classList.add("part");
 
-    part.addEventListener("mousedown", onMouseDown, {passive: false, useCapture: false});
+  part.addEventListener("mousedown", onMouseDown, {
+    passive: false,
+    useCapture: false,
+  });
 
-    const width = window.innerWidth - 1;
-    const height = window.innerHeight + 1;
+  const width = window.innerWidth - 1;
+  const height = window.innerHeight + 1;
 
-    part.style.top = `0px`;
-    part.style.left = `0px`;
-    part.style.width = `${width}px`;
-    part.style.height = `${height}px`;
+  part.style.top = `0px`;
+  part.style.left = `0px`;
+  part.style.width = `${width}px`;
+  part.style.height = `${height}px`;
 
-    //
-    // canvas
-    //
+  //
+  // canvas
+  //
 
-    //let canvas = document.createElement("canvas"); //added temp var at top
-    canvas = document.createElement("canvas");
-    //canvas.style.border = "solid 1px"
-    canvas.setAttribute("id", "canvas");
-    canvas.classList.add("canvas");
+  //let canvas = document.createElement("canvas"); //added temp var at top
+  canvas = document.createElement("canvas");
+  //canvas.style.border = "solid 1px"
+  canvas.setAttribute("id", "canvas");
+  canvas.classList.add("canvas");
 
-    canvas.setAttribute("width", width);
-    canvas.setAttribute("height", height);
+  canvas.setAttribute("width", width);
+  canvas.setAttribute("height", height);
 
-    const context = canvas.getContext("2d");
+  const context = canvas.getContext("2d");
 
-    context.fillStyle = "#E5E5E5";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "#E5E5E5";
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-    let STEP = 24;
-    for (let x = STEP; x < width - STEP + 2; x += STEP) { //this magic 2 is slightly confusing
-        for (let y = STEP; y < height - STEP; y += STEP) {
-            context.fillStyle = "#282828";
-            context.fillRect (x, y, 1, 1);
-        }
+  let STEP = 24;
+  for (let x = STEP; x < width - STEP + 2; x += STEP) {
+    //this magic 2 is slightly confusing
+    for (let y = STEP; y < height - STEP; y += STEP) {
+      context.fillStyle = "#282828";
+      context.fillRect(x, y, 1, 1);
     }
+  }
 
-    //
-    // bootstrap
-    //
-    part.appendChild(canvas);
-    main.appendChild(part);
-    document.body.appendChild(main);
-    document.body.style.overflow = "hidden"; //index cards don't scroll
+  //
+  // bootstrap
+  //
+  part.appendChild(canvas);
+  main.appendChild(part);
+  document.body.appendChild(main);
+  document.body.style.overflow = "hidden"; //index cards don't scroll
 
-    return {main, part, canvas};
+  return { main, part, canvas };
 }
 
 //
 // switch
 //
 
-function onMouseDown (evt) {
-    console.log(evt);
-    handlePartDown(evt);
-    /*
+function onMouseDown(evt) {
+  console.log(evt);
+  handlePartDown(evt);
+  /*
     if (evt.target.classList[0] == "part") {
         handlePartDown(evt);
     }
@@ -97,48 +101,54 @@ function onMouseDown (evt) {
 //
 
 function handlePartDown(evt) {
-    //default case
-    //can put "switches" in here
-    currentPosition = {x: evt.clientX, y: evt.clientY}
-    partPosition = {x: evt.clientX, y: evt.clientY}
-    document.addEventListener("mousemove", handlePartMove, {passive: false, useCapture: false});
-    document.addEventListener("mouseup", handlePartUp, {passive: false, useCapture: false});
+  //default case
+  //can put "switches" in here
+  currentPosition = { x: evt.clientX, y: evt.clientY };
+  partPosition = { x: evt.clientX, y: evt.clientY };
+  document.addEventListener("mousemove", handlePartMove, {
+    passive: false,
+    useCapture: false,
+  });
+  document.addEventListener("mouseup", handlePartUp, {
+    passive: false,
+    useCapture: false,
+  });
 
-    newpart = createElement("div",
-                            ["part"],
-                            null,
-                            [["top", `${currentPosition.y}px`],
-                             ["left", `${currentPosition.x}px`],
-                             ["width", `1px`],
-                             ["height", `1px`],
-                             ["border", "1px solid"],
-                             ["background", "#ffffff"]
-                            ]);
-
+  newpart = createElement("div", ["part"], null, [
+    ["top", `${currentPosition.y}px`],
+    ["left", `${currentPosition.x}px`],
+    ["width", `1px`],
+    ["height", `1px`],
+    ["border", "1px solid"],
+    ["background", "#ffffff"],
+  ]);
 }
 
-function handlePartMove (evt) {
-    let dx = currentPosition.x - partPosition.x;
-    let dy = currentPosition.y - partPosition.y;
+function handlePartMove(evt) {
+  let dx = currentPosition.x - partPosition.x;
+  let dy = currentPosition.y - partPosition.y;
 
-    let set = (w, h, l, t) => {
-        newpart.style.width = `${w}px`;
-        newpart.style.height = `${h}px`;
-        newpart.style.left = `${l + partPosition.x}px`;
-        newpart.style.top = `${t + partPosition.y}px`;
-    }
+  let set = (w, h, l, t) => {
+    newpart.style.width = `${w}px`;
+    newpart.style.height = `${h}px`;
+    newpart.style.left = `${l + partPosition.x}px`;
+    newpart.style.top = `${t + partPosition.y}px`;
+  };
 
-    if (dx > 1 && dy > 1) set (dx, dy, 0, 0);
-    if (dx < 1 && dy > 1) set (-dx, dy, dx, 0);
-    if (dx > 1 && dy < 1) set (dx, -dy, 0, dy);
-    if (dx < 1 && dy < 1) set (-dx, -dy, dx, dy);
+  if (dx > 1 && dy > 1) set(dx, dy, 0, 0);
+  if (dx < 1 && dy > 1) set(-dx, dy, dx, 0);
+  if (dx > 1 && dy < 1) set(dx, -dy, 0, dy);
+  if (dx < 1 && dy < 1) set(-dx, -dy, dx, dy);
 
-    currentPosition = {x: evt.clientX, y: evt.clientY};
+  currentPosition = { x: evt.clientX, y: evt.clientY };
 }
-function handlePartUp (evt) {
-    newpart.addEventListener("mousedown", onMouseDown, {passive: false, useCapture: false}); //funny
-    document.removeEventListener("mousemove", handlePartMove);
-    document.removeEventListener("mouseup", handlePartUp);
+function handlePartUp(evt) {
+  newpart.addEventListener("mousedown", onMouseDown, {
+    passive: false,
+    useCapture: false,
+  }); //funny; recursive
+  document.removeEventListener("mousemove", handlePartMove);
+  document.removeEventListener("mouseup", handlePartUp);
 }
 
 //
@@ -152,12 +162,12 @@ attributes: [[string, string]]
 styles: [[string, string]]
 */
 
-function createElement (element, classlist, attributes, styles) {
-    let thing = document.createElement(element);
-    classlist ? classlist.forEach (c => thing.classList.add(c)) : null;
-    attributes ? attributes.forEach (a => thing.setAttribute(a[0], a[1])) : null;
-    styles ? styles.forEach (s => thing.style[s[0]] = s[1]) : null;
+function createElement(element, classlist, attributes, styles) {
+  let thing = document.createElement(element);
+  classlist ? classlist.forEach((c) => thing.classList.add(c)) : null;
+  attributes ? attributes.forEach((a) => thing.setAttribute(a[0], a[1])) : null;
+  styles ? styles.forEach((s) => (thing.style[s[0]] = s[1])) : null;
 
-    document.body.appendChild(thing);
-    return thing;
+  document.body.appendChild(thing);
+  return thing;
 }
---
+++ demos/stopify09/main.js
@@ -0,0 +1,237 @@
+let state = {};
+state.instances = []; // for instance and history
+
+window.addEventListener("load", onLoad);
+
+function onLoad() {
+  let { main, layout } = bootstrap();
+}
+
+function bootstrap() {
+  let main = document.createElement("main");
+  main.setAttribute("id", "main");
+  main.classList.add("main");
+
+  let layout = document.createElement("div");
+  layout.setAttribute("id", "layout");
+  layout.classList.add("layout");
+  layout.style.display = "grid";
+  layout.style.gridTemplateColumns = "auto auto auto";
+
+  document.body.appendChild(main);
+  main.appendChild(layout);
+  document.body.style.overflow = "hidden"; //index cards don't scroll
+
+  let history = document.createElement("div");
+  history.setAttribute("id", "history");
+  history.classList.add("history");
+  history.style.border = "1px dashed #282828";
+  history.style.width = "100px";
+  history.style.height = "600px";
+  history.style.display = "flex";
+  history.style.flexDirection = "column";
+
+  layout.appendChild(history);
+
+  /* codemirror */
+
+  const cm_opts = {
+    value: "/* remember ctx!! */",
+    lineNumbers: true,
+    gutter: true, //what is this?
+    lineWrapping: true,
+    indentWithTabs: true,
+    indentUnit: 2,
+    tabSize: 2,
+    //value: '',
+    mode: "javascript",
+  };
+
+  state.codeMirror = CodeMirror(layout, cm_opts);
+
+  /* canvas */
+  let canvas = document.createElement("canvas");
+  canvas.setAttribute("id", "canvas");
+  canvas.style.border = "1px dashed #282828";
+  canvas.width = 800;
+  canvas.height = 800;
+  layout.appendChild(canvas);
+
+  state.canvas = canvas;
+
+  /* stopify */
+
+  let buttons = document.createElement("div");
+  buttons.setAttribute("id", "buttons");
+
+  let runButton = document.createElement("button");
+  runButton.setAttribute("id", "runButton");
+  runButton.innerHTML = "Build/Run";
+  let pauseButton = document.createElement("button");
+  pauseButton.setAttribute("id", "pauseButton");
+  pauseButton.innerHTML = "Pause";
+  let resumeButton = document.createElement("button");
+  resumeButton.setAttribute("id", "resumeButton");
+  resumeButton.innerHTML = "Resume";
+  let instanceButton = document.createElement("button");
+  instanceButton.setAttribute("id", "instanceButton");
+  instanceButton.innerHTML = "Instance";
+
+  runButton.addEventListener("click", build);
+  pauseButton.addEventListener("click", pause);
+  resumeButton.addEventListener("click", resume);
+  instanceButton.addEventListener("click", instance);
+
+  buttons.appendChild(runButton);
+  buttons.appendChild(pauseButton);
+  buttons.appendChild(resumeButton);
+  buttons.appendChild(instanceButton);
+
+  layout.appendChild(document.createElement("div")); // fixme hole
+
+  //none of this is working, learn css grid and how codemirror builds itself
+
+  //let cmnode = document.getElementsByClassName("CodeMirror")[0];
+  //console.log(cmnode);
+  //cmnode.appendChild(buttons);
+  layout.appendChild(buttons);
+
+  //javascript
+
+  name1 = "1: first";
+  code1 = `line(10,10,100,100,ctx);`
+  name2 = "2: clear";
+  code2 = `ctx.fillStyle = '#ffffff';\nctx.fillRect(0, 0, 800, 800);`
+  name3 = "3: line";
+  code3 = `line(10,20,100,110,ctx); //offset line`
+  inst1 = {name: name1, code: code1}
+  inst2 = {name: name2, code: code2}
+  inst3 = {name: name3, code: code3}
+  state.instances.push(initializeInstance(inst1));
+  state.instances.push(initializeInstance(inst2));
+  state.instances.push(initializeInstance(inst3));
+  engine(state);
+
+  return { main, layout };
+}
+
+/* engine */
+function engine(state) {
+  //start at 0
+  state.codeMirror.setValue(state.instances[0].code);
+  state.current = 0;
+  // clear history element state
+  let element = document.getElementById("history");
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+
+  state.current = 0; //the first instance in instances
+  state.instances.forEach ((e,i) => {
+    let button = document.createElement("button");
+    button.innerHTML = e.name;
+    button.addEventListener("click", function() {
+      //todo: sets codemirror
+      // sets this instance...
+      state.codeMirror.setValue(state.instances[i].code);
+      state.current = i;
+    });
+    let history = document.getElementById("history");
+    history.appendChild(button);
+  })
+}
+
+function initializeInstance({name, code}) {
+  let instance = {name, code};
+  //instance.name = "first";
+  //instance.code = `line(10, 10, 100, 100, ctx)`
+  //instance.runner = stopify.stopifyLocally(instance.code); //this might not make sense
+  //instance.globals = ;
+  return instance;
+}
+
+/* stopify */
+
+//
+//  BUTTON
+//
+function build() {
+  // set task variable
+  let instance = state.instances[state.current];
+  instance.code = state.codeMirror.getValue();
+  let runner = stopify.stopifyLocally(instance.code);
+
+  /* canvas */
+  const ctx = state.canvas.getContext("2d");
+
+  runner.g = {
+    window,
+    //runner,
+    main,
+    layout,
+    document,
+    ctx,
+    console,
+    line,
+    point,
+    sleep,
+    Math,
+  };
+  if (instance.runner) delete instance.runner;
+  instance.runner = runner;
+  console.log(runner);
+  run();
+}
+
+function run() {
+  //runner.run((result) => console.log(result));
+  let target = state.instances[state.current];
+  target.runner.run((result) => console.log(result));
+}
+
+function pause() {
+  //runner.pause(() => console.log("pause"));
+  state.instances[state.current].runner.pause(() => console.log("paused"));
+}
+
+function resume() {
+  //runner.resume(); //no callback it looks like
+  state.instances[state.current].runner.resume();
+}
+
+function instance() {
+  state.instances.push(state.codeMirror.getValue());
+
+  let button = document.createElement("button");
+  button.classList.add("instances");
+  button.innerHTML = state.instances.length - 1;
+  button.dataset.value = state.instances.length - 1;
+
+  button.addEventListener("click", function () {
+    state.codeMirror.setValue(state.instances[this.dataset.value]);
+  });
+
+  let history = document.getElementById("history");
+  history.appendChild(button);
+}
+
+/* canvas/runner utility */
+
+function point(x, y, ctx) {
+  ctx.fillRect(x, y, 1, 1);
+}
+
+function line(x1, y1, x2, y2, ctx) {
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+}
+
+function sleep(duration, runner) {
+  runner.pauseImmediate(() => {
+    window.setTimeout(
+      () => runner.continueImmediate({ type: "normal", value: undefined }),
+      duration
+    );
+  });
+}
